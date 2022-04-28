2d1918412f48b23ed3a4bd8c12c3d5ec93f20708
Pedro Augusto Bragheto da Costa <47837910+Pedrxx@users.noreply.github.com>
2022-04-27 22:20:57 -0300

    Aula 27-04

---
+++ Empresa.java
@@ -0,0 +1,25 @@
+public class Empresa {
+
+    private String nome;
+    private int patrimonio;
+ 
+    public String setNome(String nome) {
+    this.nome = nome;
+    }
+
+    public int setPatrimonio(int patrimonio) {
+    this.patrimonio = patrimonio;
+    return patrimonio;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int setNome() {
+        return this.patrimonio;
+    }
+
+}
+
+
---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        Empresa prats = new Empresa();
+
+        prats.setNome("SUCO PRATS");
+
+        System.out.println(prats.getNome);
+        
+    }
+
+}
